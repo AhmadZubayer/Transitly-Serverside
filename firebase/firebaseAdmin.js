17beaa5dbc838c6d891c2d13cd7c63313667587f
AhmadZubayer <ahmadzubayer369@gmail.com>
2026-05-08 16:07:34 +0600

    fixed firebase Admin issue

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -1,11 +1,6 @@
 const path = require('path');
 const admin = require('firebase-admin');
 
-// Keep service account JSON in serverside root as: transitly-firebase-adminsdk.json
-// const serviceAccountPath = path.join(__dirname, '..', 'transitly-firebase-adminsdk.json');
-// // eslint-disable-next-line import/no-dynamic-require, global-require
-// const serviceAccount = require(serviceAccountPath);
-
 const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
 const serviceAccount = JSON.parse(decoded);
 
